de2edd9063482c21d5f998f130544669a5ea5250
sagittarius-singularity <sagittarius.singularity@gmail.com>
2026-07-05 01:53:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@
 # The website is actually intented for 8th to 9th Grade | Middle to hight school.
 # Our goal is to offer free online mathematics revision, as well as advanced lessons covering the high school curriculum.
 # The images used are not protected.
-
+# ============================================
+# --------------------------------------------
 
 from flask import Flask, render_template
 
