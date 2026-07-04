066cdaa0dc129cb09a1c3fb36d79c4196e69febc
sagittarius-singularity <sagittarius.singularity@gmail.com>
2026-07-05 01:48:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+# --------------------------------------------
+# ============================================
+# © Maths Expert 2026 | All rights reserverd.
+# This online mathematics learning site was created by Pablo Koussa Diaz and Noé Touati.
+# This repository may not be copied; any copying will result in legal action.
+# The website is actually intented for 8th to 9th Grade | Middle to hight school.
+# Our goal is to offer free online mathematics revision, as well as advanced lessons covering the high school curriculum.
+# The images used are not protected.
+
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
