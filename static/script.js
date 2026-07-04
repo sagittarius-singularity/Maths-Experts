e4c8ace797b8a90b77f98cb0da072d7eef283a4f
sagittarius-singularity <sagittarius.singularity@gmail.com>
2026-07-05 01:52:38 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,3 +1,11 @@
+/* © Maths Expert 2026 | All rights reserverd. */
+/* This online mathematics learning site was created by Pablo Koussa Diaz and Noé Touati. */
+/* This repository may not be copied; any copying will result in legal action. */
+/* The website is actually intented for 8th to 9th Grade | Middle to hight school. */
+/* Our goal is to offer free online mathematics revision, as well as advanced lessons covering the high school curriculum. */
+/* The images used are not protected. */
+
+
 const input_name = document.getElementById("input-name");
 const checkicon = document.getElementById("check");
 const continueBtn = document.getElementById("continueBtn");
